0950bad2c1fa495aad5a84cf3aafece4f776b108
edricklarkin <edricklarkin@gmail.com>
2024-07-30 16:26:15 -0400

    Initial committ

---
+++ get_secret.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 09:26:44 2024
+
+Example process to use AWS Secrets Manager via roles anywhere to retreive secrets
+Does not require aws_signing_helper application
+
+"""
+
+import json
+from dotenv import dotenv_values
+
+#call .env file with non-sensitive AWS details
+env_val = dotenv_values(".env")
+
+#secret id to retreive
+secret_id = 'test-secret'
+
+from iam_rolesanywhere_session import IAMRolesAnywhereSession
+
+roles_anywhere_session = IAMRolesAnywhereSession(
+    profile_arn=env_val.get('PROFILE_ARN'),
+    role_arn=env_val.get('ROLE_ARN'),
+    trust_anchor_arn=env_val.get('TRUST_ANCHOR_ARN'),
+    certificate=env_val.get('CERT_PATH'),
+    private_key=env_val.get('PRIVATE_KEY_PATH'),
+    region='us-east-1',
+).get_session()
+
+client = roles_anywhere_session.client('secretsmanager')
+
+#client returns a dict of strings
+secret_string = client.get_secret_value(SecretId=secret_id)['SecretString']
+
+secret_dict = json.loads(secret_string)
+
+user_name = secret_dict['username']
+password = secret_dict['password']
+
+print(f'Username is {user_name} and password is {password}.')
---
+++ rotate_secret.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:36:41 2024
+
+Example process for rotating secrets for an on-premises service
+Secrets stored to AWS Secrets Manager
+
+NOTE: this example assumes the user has authority to change own password
+
+"""
+
+import random
+import string
+import MySQLdb
+import json
+from dotenv import dotenv_values
+from iam_rolesanywhere_session import IAMRolesAnywhereSession
+
+#call .env file with non-sensitive AWS details
+env_val = dotenv_values(".env")
+
+#global variables
+secret_name = 'test-secret'
+
+#connect to secrets manager
+roles_anywhere_session = IAMRolesAnywhereSession(
+    profile_arn=env_val.get('PROFILE_ARN'),
+    role_arn=env_val.get('ROLE_ARN'),
+    trust_anchor_arn=env_val.get('TRUST_ANCHOR_ARN'),
+    certificate=env_val.get('CERT_PATH'),
+    private_key=env_val.get('PRIVATE_KEY_PATH'),
+    region='us-east-1'
+    ).get_session()
+
+client = roles_anywhere_session.client('secretsmanager',verify=False)
+
+#create random password
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+#get database credentials from AWS Secrets Manager
+def get_db_credentials():
+    
+    return json.loads(client.get_secret_value(SecretId=secret_name)['SecretString'])
+ 
+#change password
+current_secret = get_db_credentials()
+new_secret = dict(current_secret)
+new_secret['password'] = generate_password(12)
+
+
+#connect to database
+db = MySQLdb.connect(host=current_secret['host'],
+                                user=current_secret['username'],
+                                passwd=current_secret['password'], 
+                                db='mysql')
+
+#execute password change code and close dabase connection 
+cur = db.cursor()
+cur.execute(f"ALTER USER '{new_secret['username']}'@'%' IDENTIFIED BY '{new_secret['password']}';")
+db.close()
+
+#change password in AWS Secret Manager
+client.update_secret(SecretId=secret_name, 
+                     SecretString=json.dumps(new_secret))
+
+
